37e608b5e04f29871ca4c1ba0185882d3e4178bd
SolalDR <solal.dussout-revel@hotmail.fr>
2022-03-18 17:48:31 +0100

    wip

--- saofile.js
+++ saofile.js
@@ -1,6 +1,37 @@
-const { merge, sortByKey } = require('./util')
+// const { merge, sortByKey } = require('./util')
 
 console.log("Hello", _dirname, __dirname)
+
+function isObject (value)  {
+  return !!value &&
+  typeof value === 'object' &&
+  typeof value.getMonth !== 'function' &&
+  !Array.isArray(value)
+}
+
+function merge (...sources) {
+  const [target, ...rest] = sources
+
+  for (const object of rest) {
+    for (const key in object) {
+      const targetValue = target[key]
+      const sourceValue = object[key]
+      const isMergable = isObject(targetValue) && isObject(sourceValue)
+      target[key] = isMergable ? merge({}, targetValue, sourceValue) : sourceValue
+    }
+  }
+
+  return target
+}
+
+function sortByKey (unsortedObject)  {
+  const sortedObject = {}
+  Object.keys(unsortedObject).sort().forEach((key) => {
+    sortedObject[key] = unsortedObject[key]
+  })
+  return sortedObject
+}
+
 
 function requireFile (filename) {
   try {
