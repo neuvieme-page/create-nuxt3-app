e5f0e23ebaaf4ecd7873e1acdeb407d49605bbcf
SolalDR <solal.dussout-revel@hotmail.fr>
2022-03-18 20:00:26 +0100

    wip

--- cli.js
+++ cli.js
@@ -4,10 +4,30 @@ const path = require('path')
 const cac = require('cac')
 const fs = require('fs')
 const chalk = require('chalk')
+var { execSync } = require('child_process')
 const envinfo = require('envinfo')
 const { version } = require('./package.json')
 
 const generator = path.resolve(__dirname, './')
+
+
+const list = execSync('ls')
+console.log(list.toString())
+
+//passsing directoryPath and callback function
+fs.readdir(__dirname, function (err, files) {
+  //handling error
+  if (err) {
+      return console.log('Unable to scan directory: ' + err);
+  }
+  //listing all files using forEach
+  files.forEach(function (file) {
+      // Do whatever you want to do with the file
+      console.log(file);
+  });
+});
+
+
 
 const showEnvInfo = async () => {
   console.log(chalk.bold('\nEnvironment Info:'))
